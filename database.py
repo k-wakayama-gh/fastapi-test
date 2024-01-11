f65e2520c4b1b0adff4d2b991b14ae3dbacd97d9
k-wakayama-gh <119306795+k-wakayama-gh@users.noreply.github.com>
2024-01-12 03:51:01 +0900

    Add files via upload

--- database.py
+++ database.py
@@ -2,9 +2,17 @@
 
 # modules
 from sqlmodel import SQLModel, create_engine, Session
+import os
+
+volume_path = "/code/volume1"
+
+if not os.path.exists(volume_path):
+    os.makedirs(volume_path)
+
+
 
 # database settings
-engine = create_engine('sqlite:///database.sqlite', echo=False, connect_args={'check_same_thread': False})
+engine = create_engine('sqlite:///volume1_database.sqlite', echo=False, connect_args={'check_same_thread': False})
 
 # def : create the database
 def create_database():
